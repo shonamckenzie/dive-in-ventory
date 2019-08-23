24976d42c266928767b35837f56b2b7dfde16b05
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-08-23 18:07:09 +0100

    create initial specs files

---
+++ specs/manufacturer_spec.rb
@@ -0,0 +1,17 @@
+require('minitest')
+require('minitest/autorun')
+require_relative('../models/manufacturer')
+
+class ManufacturerTest < MiniTest::Test
+
+
+
+
+
+
+
+
+
+
+
+end 
---
+++ specs/product_spec.rb
@@ -0,0 +1,16 @@
+require('minitest')
+require('minitest/autorun')
+require_relative('../models/product')
+
+class ProductTest < MiniTest::Test
+
+
+
+
+
+
+
+
+
+
+end 
