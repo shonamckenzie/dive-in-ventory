0f21b815d9c6bb665f63a432fc8903d60578f367
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-08-23 17:58:03 +0100

    create db structure and seeds file

---
+++ db/mikes.sql
@@ -0,0 +1,20 @@
+DROP TABLE products;
+DROP TABLE manufacturers;
+
+CREATE TABLE manufacturers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  country VARCHAR(255),
+  contact VARCHAR(255),
+  rating INT4
+);
+
+CREATE TABLE products (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  quantity INT4,
+  buy_cost INT8,
+  sell_price INT8,
+  manufacturer_id INT4 REFERENCES manufacturers(id) ON DELETE SET NULL
+);
