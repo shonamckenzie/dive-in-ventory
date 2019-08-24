9bc947e9348467169c8ac89cde7e5502ef1e6eb3
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-08-24 15:22:26 +0100

    start working through sinatra setup

---
+++ products_controller.rb
@@ -0,0 +1,6 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('../models/product')
+require_relative('../models/manufacturer')
+also_reload( '../models/*' )
